9e481c0841ec4443ff48a799840d1ccf0e78fc70
vladovinkin@book <vladovinkin@gmail.com>
2024-03-03 11:15:59 +0300

    убрал из заголовочного файла удалённую функцию

--- lab1/Replace/Replace.h
+++ lab1/Replace/Replace.h
@@ -2,5 +2,4 @@
 #include <string>
 
 void CopyStreamWithReplace(std::ifstream& input, std::ofstream& output, std::string searchSubstr, std::string repalceSubstr);
-void StreamProcessedLine(std::string line, std::ofstream& output, std::string searchSubstr, std::string replaceSubstr);
 std::string GetOneLineReplaced(std::string line, std::string searchSubstr, std::string replaceSubstr);
