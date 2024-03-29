e317c840e318353a1f8603f99c0460e549676830
vladovinkin@book <vladovinkin@gmail.com>
2024-03-29 22:53:34 +0300

    сохранил в UTF-8 добавил Post-Build обработку

--- lab1/Invert/Invert.cpp
+++ lab1/Invert/Invert.cpp
@@ -1,4 +1,4 @@
-// Invert.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// Invert.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
 #include <fstream>
