6d6f35b1b1882f05f764f724432d0d84051f5d5e
vladovinkin@book <vladovinkin@gmail.com>
2024-06-27 00:15:38 +0300

    - добавил методы Push() и Size(); - написал тест для новых методов

--- lab7/MyStack/CMyStack/CMyStack.h
+++ lab7/MyStack/CMyStack/CMyStack.h
@@ -5,42 +5,67 @@ template <typename T>
 class CMyStack
 {
 public:
-	CMyStack();
-	~CMyStack();
+	CMyStack() noexcept;
+	~CMyStack() noexcept;
 	bool Empty() const;
-	/*void Push(const T value);
+	T& Top() const;
+	void Push(const T& value);
+	int Size()const;
+	/*
 	void Pop();
-	T& Top();
 	void Clear();*/
 private:
 	template<typename Ty>
 	struct Stack_Node {
-		Ty data;
-		Stack_Node* next;
-		Stack_Node(const Ty& _data, const Stack_Node* _next) :data((Ty)_data), next((Stack_Node*)_next) {}
+		Ty m_data;
+		Stack_Node* m_next;
+		Stack_Node(const Ty& _data, const Stack_Node* _next) :m_data((Ty)_data), m_next((Stack_Node*)_next) {}
 	};
-	Stack_Node<T>* top;
-	int size;
+	Stack_Node<T>* m_top;
+	int m_size;
 };
 
 template <typename T>
-CMyStack<T>::CMyStack()
-	:top(nullptr), size(0)
+CMyStack<T>::CMyStack() noexcept
+	:m_top(nullptr), m_size(0)
 {}
 
 template <typename T>
-CMyStack<T>::~CMyStack()
+CMyStack<T>::~CMyStack() noexcept
 {
-	Stack_Node<T>* temp = top;
-	while (top) {
-		top = top->next;
+	Stack_Node<T>* temp = m_top;
+	while (m_top) {
+		m_top = m_top->m_next;
 		delete temp;
-		temp = top;
+		temp = m_top;
 	}
 }
 
 template <typename T>
 bool CMyStack<T>::Empty()const
 {
-	return size == 0;
+	return m_size == 0;
+}
+
+template<typename T>
+T& CMyStack<T>::Top()const 
+{
+	if (Empty())
+	{
+		throw std::logic_error("Stack is empty");
+	}
+	return m_top->m_data;
+}
+
+template<typename T>
+void CMyStack<T>::Push(const T& value) 
+{
+	m_top = new Stack_Node<T>(value, m_top);
+	++m_size;
+}
+
+template<typename T>
+int CMyStack<T>::Size()const
+{
+	return m_size;
 }
--- lab7/MyStack/MyStackTest/MyStackTest.cpp
+++ lab7/MyStack/MyStackTest/MyStackTest.cpp
@@ -9,5 +9,18 @@ TEST_CASE("Create empty stack")
 
 	CMyStack<int> stack = CMyStack<int>();
 
-	REQUIRE(stack.Empty() == true);
+	CHECK(stack.Empty() == true);
+}
+
+TEST_CASE("Push two elements, size mus be = 2, top must be second element")
+{
+	setlocale(LC_ALL, "rus");
+
+	CMyStack<int> stack = CMyStack<int>();
+	stack.Push(531);
+	stack.Push(-198);
+
+	CHECK(stack.Empty() == false);
+	CHECK(stack.Top() == -198);
+	CHECK(stack.Size() == 2);
 }
